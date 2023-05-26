130588bfbb30c92d18cced63d031d012bbe0cbf5
raudaholamide <123153259+raudaholamide@users.noreply.github.com>
2023-05-26 13:38:54 +0100

    Update errors1.c

--- errors1.c
+++ errors1.c
@@ -1,67 +1,57 @@
-#include "monty.h"
-
-int usage_error(void);
-int malloc_error(void);
-int f_open_error(char *filename);
-int unknown_op_error(char *opcode, unsigned int line_number);
-int no_int_error(unsigned int line_number);
-
+#include"monty.h"
 /**
- * usage_error - Prints usage error messages.
- *
- * Return: (EXIT_FAILURE) always.
- */
-int usage_error(void)
+ * error_arguments -Main entry
+ *Description: If user doesnt give file or more than valid arguments
+ * Return: void
+ **/
+void error_arguments(void)
 {
 	fprintf(stderr, "USAGE: monty file\n");
-	return (EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 }
-
 /**
- * malloc_error - Prints malloc error messages.
- *
- * Return: (EXIT_FAILURE) always.
- */
-int malloc_error(void)
+ * open_error -Main entry
+ *Description: print message if its not possible to open the file
+ * @argv: arguments received by main, ni this case te filename
+ * Return: void
+ **/
+void open_error(char **argv)
+{
+	char *filename = argv[1];
+
+	fprintf(stderr, "Error: Can't open file %s\n", filename);
+	exit(EXIT_FAILURE);
+}
+/**
+ * invalidInstruction_error -Main entry
+ *Description: print message if le to open the filehas an invalid instruction
+ * @invInstruction: instruction
+ * @line: line
+ * Return: void
+ **/
+void invalidInstruction_error(char *invInstruction, unsigned int line)
+{
+	fprintf(stderr, "L%u: unknown instruction %s\n", line, invInstruction);
+	exit(EXIT_FAILURE);
+}
+/**
+ * not_int_err -Main entry
+ *Description: print message if parameter received is not integer
+ * @line: line
+ * Return: void
+ **/
+void not_int_err(unsigned int line)
+{
+	fprintf(stderr, "L%u: usage: push integer\n", line);
+	exit(EXIT_FAILURE);
+}
+/**
+ * malloc_error -Main entry
+ *Description: print message if cant malloc anymore
+ * Return: void
+ **/
+void malloc_error(void)
 {
 	fprintf(stderr, "Error: malloc failed\n");
-	return (EXIT_FAILURE);
-}
-
-/**
- * f_open_error - Prints file opening error messages w/ file name.
- * @filename: Name of file failed to open
- *
- * Return: (EXIT_FAILURE) always.
- */
-int f_open_error(char *filename)
-{
-	fprintf(stderr, "Error: Can't open file %s\n", filename);
-	return (EXIT_FAILURE);
-}
-
-/**
- * unknown_op_error - Prints unknown instruction error messages.
- * @opcode: Opcode where error occurred.
- * @line_number: Line number in Monty bytecodes file where error occured.
- *
- * Return: (EXIT_FAILURE) always.
- */
-int unknown_op_error(char *opcode, unsigned int line_number)
-{
-	fprintf(stderr, "L%u: unknown instruction %s\n",
-		line_number, opcode);
-	return (EXIT_FAILURE);
-}
-
-/**
- * no_int_error - Prints invalid monty_push argument error messages.
- * @line_number: Line number in Monty bytecodes file where error occurred.
- *
- * Return: (EXIT_FAILURE) always.
- */
-int no_int_error(unsigned int line_number)
-{
-	fprintf(stderr, "L%u: usage: push integer\n", line_number);
-	return (EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 }
